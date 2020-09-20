72db7a85368af1b67d02b2475e0553897c2e2ae1
Isaac Pugliese <isaacpugliese2010@gmail.com>
2020-09-20 14:21:41 -0300

    OlaUsuario Terminado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace olausuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Por Favor Digite seu Nome: ");
+            string NmUsuario = Console.ReadLine();
+            Console.Clear();
+            Console.Write("Olá, ");
+            Console.WriteLine(NmUsuario);
+        }
+    }
+}
